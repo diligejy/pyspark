3220f7524c5eaafc1c155c368af39ab4512747b1
diligejy <sjy049@gmail.com>
2020-06-29 04:31:18 +0000

    zip까지

--- spark2_big/ch2/practice.py
+++ spark2_big/ch2/practice.py
@@ -14,7 +14,7 @@ class Practice():
 
     
     def doMapPartitions(self, sc):
-        
+
         # increase
         def increase(numbers):
             print("DB 연결!!!")
@@ -24,7 +24,35 @@ class Practice():
         rdd2 = rdd1.mapPartitions(increase)
         print(rdd2.collect())
     
+    def doMapPartitionsWithIndex(self, sc):
+    
+        # increaseWithIndex
+        def increaseWithIndex(idx, numbers):
+            for i in numbers:
+                if (idx == 1):
+                    yield i + 1
+    
+        rdd1 = sc.parallelize(range(1, 11), 3)
+        rdd2 = rdd1.mapPartitionsWithIndex(increaseWithIndex)
+        print(rdd2.collect())
+    
+    def doMapValues(self, sc):
+        rdd1 = sc.parallelize(["a", "b", "c"])
+        # (키, 값) 쌍으로 구성된 RDD를 생성
+        rdd2 = rdd1.map(lambda v : (v, 1))
+        rdd3 = rdd2.mapValues(lambda i : i + 1)
+        print(rdd3.collect())
 
+    def doFlatMapValues(self, sc):
+        rdd1 = sc.parallelize([(1, "a, b"), (2, "a, c"), (1, "d, e")])
+        rdd2 = rdd1.flatMapValues(lambda s : s.split(","))
+        print(rdd2.collect())
+
+    def doZip(self, sc):
+        rdd1 = sc.parallelize(["a", "b", "c"])
+        rdd2 = sc.parallelize([1, 2, 3])
+        result = rdd1.zip(rdd2)
+        print(result.collect())
 
 if __name__ == "__main__":
     conf = SparkConf()
@@ -32,6 +60,9 @@ if __name__ == "__main__":
     sc = SparkContext(master="local[*]", appName="Practice", conf = conf)
     obj = Practice()
     
-    obj.doMapPartitions(sc)
-    
-    # obj.doFlatMap(sc)
+    #obj.doFlatMap(sc)
+    #obj.doMapPartitions(sc)
+    #obj.doMapPartitionsWithIndex(sc)
+    #obj.doMapValues(sc)
+    #obj.doFlatMapValues(sc)
+    obj.doZip(sc)
